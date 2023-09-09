4af83eb76fd6a258659a4e1b7baf1672befde572
Andres <andresr10020@gmail.com>
2023-09-08 19:24:22 -0500

    save input + add category

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -4,11 +4,24 @@ import "../src/styles.css";
 const apiKey = "zgpzLecnpi3Gqk6ae7Dko6eO0Mv95laR";
 
 export const GiftExpertApp = () => {
-
-        
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+  let valor = "";
+  const addNewCategory = (valor) => {
+    console.log(1);
+    valor != "" && setCategories([...categories, valor]);
+    console.log(categories);
+  };
+  console.log(categories);
   return (
     <>
       <div>Gift Expert App</div>
+      <input type="text" onChange={(e) => (valor = e.target.value)} />
+      <button onClick={() => addNewCategory(valor)}>Agregar</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };
