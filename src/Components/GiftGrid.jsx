177aaaf97f0c3f39ba30173a9320d8a68dd60374
Andres <andresr10020@gmail.com>
2023-09-12 21:43:57 -0500

    save grd1

--- src/Components/GiftGrid.jsx
+++ src/Components/GiftGrid.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
+
 export const GiftGrid = ({ category, apiKey }) => {
-  const urlGif = "api.giphy.com/v1/gifs/search";
-  const urlSticket = "api.giphy.com/v1/stickers/search";
-  console.log(1);
-  console.log(category);
+  const urlGif = "https://api.giphy.com/v1/gifs/search";
+  const urlSticket = "https://api.giphy.com/v1/stickers/search";
+  const [imagenes, setImagenes] = useState([]);
+  const getImg = async () => {
+    try {
+      console.log(1);
+      const promise = await fetch(
+        `${urlGif}?q=${category}&limit=8&api_key=${apiKey}`
+      );
+      const { data } = await promise.json();
+      const image = data.map((e) => e.images.original.url);
+
+      setImagenes(image);
+      console.log(imagenes);
+    } catch (error) {
+      return error;
+    }
+  };
+
+  getImg();
+  console.log(imagenes);
   return (
-    <div key={category}>
+    <div key={imagenes}>
       <h3>{category}</h3>
-      <ul></ul>
+
+      {imagenes.map((e) => (
+        <img src={e} alt="" />
+      ))}
     </div>
   );
 };
