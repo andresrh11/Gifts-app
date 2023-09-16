412c4818abc2fba87a8bb13a0a320e10de60f998
Andres <andresr10020@gmail.com>
2023-09-15 22:17:05 -0500

    save fix

--- src/Components/GiftGrid.jsx
+++ src/Components/GiftGrid.jsx
@@ -4,6 +4,7 @@ const urlGif = "https://api.giphy.com/v1/gifs/search";
 const urlSticket = "https://api.giphy.com/v1/stickers/search";
 
 export const GiftGrid = ({ category, apiKey }) => {
+  let i = 1;
   const [imagenes, setImagenes] = useState([]);
   useEffect(() => {
     console.log(1);
@@ -11,13 +12,16 @@ export const GiftGrid = ({ category, apiKey }) => {
   }, []);
 
   return (
-    <div key={imagenes}>
+    <div>
       <h3>{category}</h3>
-      <div className="grilla">
-        {imagenes.map((e) => (
-          <img key={e.id} src={e.url} alt="" />
-        ))}
-      </div>
+      {imagenes.map((e) => (
+        <div key={e.id} className="grilla">
+          <ol>
+            <img src={e.url} alt="" key={e.id} />
+            <li key={imagenes.id}> {e.title}</li>
+          </ol>
+        </div>
+      ))}
     </div>
   );
 };
